87ca7b16d41a84f4cc64d362499507cbe8564ad5
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-09-12 12:47:40 -0500

    Promise wasn't defined here too.

--- models/mixins/PresentationResources.js
+++ models/mixins/PresentationResources.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var Promise = global.Promise || require('es6-promise').Promise;
+
 var merge = require('merge');
 var isBrowser = require('../../utils/browser');
 var urlJoin = require('../../utils/urljoin');
