86a1f3d6222ae4deaa361b9db15775f40faedda6
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-09-24 12:14:03 -0500

    getTranscript method

--- models/Video.js
+++ models/Video.js
@@ -1,8 +1,13 @@
 'use strict';
 
+var Promise = global.Promise || require('es6-promise').Promise;
+
 var merge = require('merge');
 var withValue = require('../utils/object-attribute-withvalue');
+var isEmpty = require('../utils/isempty');
 
+var NO_TRANSCRIPT = 'No Transcript';
+var NO_TRANSCRIPT_LANG = 'No Transcript for the requested language.';
 
 function Video(service, data, parent) {
 	Object.defineProperty(this, '_service', withValue(service));
@@ -13,9 +18,32 @@ function Video(service, data, parent) {
 
 merge(Video.prototype, {
 
+	/**
+	 * @param {String} [lang] Request a language specific transcript. If
+	 *                        nothing is provided, it will default to english.
+	 * @return {Promise}
+	 */
+	getTranscript: function(lang) {
+		var target = lang || 'en';
+		var scripts = this.transcripts;
+
+		if (isEmpty(scripts)) {
+			return Promise.reject(NO_TRANSCRIPT);
+		}
+
+		function find(result, potential) {
+			return result || (potential.lang === target && potential);
+		}
+
+		target = this.transcripts.reduce(find, null);
+		if (!target) {
+			return Promise.reject(NO_TRANSCRIPT_LANG);
+		}
+
+		return this._service.get(target.src);
+	}
 
 });
-
 
 
 function parse(service, data, parent) {
@@ -26,6 +54,10 @@ function parse(service, data, parent) {
 	return new Video(service, data, parent);
 }
 
+
+//Static defs
+Video.NO_TRANSCRIPT = NO_TRANSCRIPT;
+Video.NO_TRANSCRIPT_LANG = NO_TRANSCRIPT_LANG;
 Video.parse = parse.bind(Video);
 
 module.exports = Video;
