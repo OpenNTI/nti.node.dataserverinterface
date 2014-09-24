90b0bd1340b2b7a62e3bb19199c50de6ce9cad7f
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-09-24 16:54:55 -0500

    add utility

---
+++ utils/function-call.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * Calls a function with args. If the passed function is falsy nothing happens.
+ *
+ * @param  {Function} fn   Function to call.
+ * @param  {any...}   [args...] Varargs of paramaters.
+ * @return {*}
+ */
+module.exports = function call(fn, args) {
+	var params = Array.prototype.slice.call(arguments);
+
+	params.shift();//drop the function off
+
+	return fn && fn.apply(null, params);
+};
