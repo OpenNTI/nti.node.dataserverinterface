569427fc0bca3e1da4cfda685f5a980488a330c5
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-09-29 13:50:11 -0500

    add utility

---
+++ utils/forcehost.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Url = require('url');
+
+module.exports = exports = function forceHost(s) {
+	//Force our config to always point to our server...(client side)
+	var url = Url.parse(s);
+	url.host = null;
+	url.hostname = location.hostname;
+	return url.format();
+};
