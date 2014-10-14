b3c4655d5ec00b0b6d95fe44c1d7a7493a8787f7
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-10-13 22:51:51 -0500

    I need unit tests.

--- utils/forcehost.js
+++ utils/forcehost.js
@@ -5,8 +5,9 @@ var Url = require('url');
 module.exports = exports = function forceHost(s) {
 	//Force our config to always point to our server...(client side)
 	var url = Url.parse(s);
-	url.host = null;
+	url.host = global.location.host;
 	url.hostname = global.location.hostname;
 	url.protocol = global.location.protocol;
+	url.port = global.location.port;
 	return url.format();
 };
