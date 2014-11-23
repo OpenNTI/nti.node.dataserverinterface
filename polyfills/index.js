fda7469fc07ffbcbb6973e3e32a09e8bcb9d051b
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-11-23 14:05:59 -0600

    polyfill console apis

--- polyfills/index.js
+++ polyfills/index.js
@@ -1,5 +1,35 @@
 'use strict';
 
+function applyIf(dest, src) {
+	for(var i in src) {
+		if (src.hasOwnProperty(i)) {
+			if (!dest.hasOwnProperty(i)) {
+				dest = src[i];
+			}
+		}
+	}
+}
+
+var noop = function() {};
+
+if (!global.console) {
+	global.console = { log: noop };
+}
+
+applyIf(console, {
+	debug: console.log,
+	info: console.log,
+	warn: console.log,
+	error: console.log,
+	group: noop,
+	trace: noop,
+	groupCollapsed: noop,
+	groupEnd: noop,
+	time: noop,
+	timeEnd: noop
+});
+
+
 if (!global.Promise) {
 	global.Promise = require('es6-promise').Promise;
 }
