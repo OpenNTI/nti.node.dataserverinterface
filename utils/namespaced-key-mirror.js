9de3ac2f2f0b38994de8bdcbf8b7b9f4eabd14a6
Ray Hatfield <ray.hatfield@nextthought.com>
2014-12-08 17:47:53 -0600

    added doc comments.

--- utils/namespaced-key-mirror.js
+++ utils/namespaced-key-mirror.js
@@ -5,6 +5,11 @@
 * For example:
 *	namespacedKeyMirror('example', { key1: null, key2: null });
 * 	{key1: 'example:key1', key2: 'example:key2'}
+* 
+*  @param {String} namespace - values will be prefixed with this string. if not provided
+*	returns an object with values equal to keys.
+*  @param {Object} object - Object with keys to be mirrored.
+*  @param {String} separator - the separator to be used between the namespace and value; defaults to ':'
 */
 module.exports = function namespacedKeyMirror(namespace, object, separator) {
 	var separator = separator||':';
