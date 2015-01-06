6fa95775ed4bd3f19cf50fc6d73bab0158d1fbfa
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-01-06 13:57:40 -0600

    WTF, what were we thinking with ShortAnswer's?? :P
    it hijact's the content for input purposes... content is readonly/static. Input... is... i just can't. /rant

--- models/assessment/parts/FillInTheBlank.js
+++ models/assessment/parts/FillInTheBlank.js
@@ -1,9 +1,23 @@
 'use strict';
 
-
 var Base = require('../Part');
 
+var isShortAnswer = RegExp.prototype.test.bind(/ShortAnswer/i);
+var hasInputs = RegExp.prototype.test.bind(/<input/i);
+
 function FillInTheBlank(service, parent, data) {
+	if (isShortAnswer(data.MimeType)) {
+		// FillInTheBlankShortAnswer is F'd up... the content has input boxes in many
+		// instances (possibly all instances). That should be illegal. The text with
+		// interspersed inputs should ONLY in the 'input' field.
+		// So, if we detect that the content has <input tags, prefix the content to
+		// the input field, and blank out the content field.
+		if (hasInputs(data.content)) {
+			data.input = data.content + ' ' + (data.input || '');
+			data.content = '';
+		}
+	}
+
 	Base.call(this, service, parent, data);
 }
 
