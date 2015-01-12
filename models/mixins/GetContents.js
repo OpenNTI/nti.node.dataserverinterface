bd86ed76ec3d4bf05a59b4810c9c5766749cd08c
Ray Hatfield <ray.hatfield@nextthought.com>
2015-01-12 04:41:51 -0600

    Add support for query parameters.

--- models/mixins/GetContents.js
+++ models/mixins/GetContents.js
@@ -1,14 +1,17 @@
 'use strict';
 
 var parse = require('../../utils/parse-object');
+var toQueryString = require('../../utils/object-to-querystring');
 
 module.exports = {
-	getContents: function () {
+	getContents: function (params) {
 		var link = this.getLink('contents');
 		if (!link) {
 			return Promise.reject('No Link!?');
 		}
-
+		if (typeof params === 'object') {
+			link = link.concat('?',toQueryString(params));
+		}
 		return this._service.get(link)
 			.then(function(wrapper){
 				wrapper.Items = parse(this, wrapper.Items);
