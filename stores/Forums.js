e55a7d6f92ad443af23b3236dd5860ff68f86bea
Ray Hatfield <ray.hatfield@nextthought.com>
2015-01-21 18:00:32 -0600

    Forums interface grows a reportItem method.

--- stores/Forums.js
+++ stores/Forums.js
@@ -18,6 +18,14 @@ Object.assign(Forums.prototype, {
 		ntiid = NTIID.decodeFromURI(ntiid);
 		return this._service.getObject(ntiid)
 			.then(parser.bind(this, this));
+	},
+
+	reportItem: function(o) {
+		var link = o && o.getLink && o.getLink('flag') || o.getLink('flag.metoo');
+		if (!link) {
+			return Promise.reject('Item has neither flag nor flag.metoo link.');
+		}
+		return this._service.post(link).then(parser.bind(this, this));
 	}
 });
 
